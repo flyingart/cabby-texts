93e6424f11801b785255f7c3371d38ab1c37ee22
FlyingArt <flyingartdev@gmail.com>
2025-10-13 10:58:41 +0200

    update timeoout for crew-safety-briefing

--- texts.ts
+++ texts.ts
@@ -1094,7 +1094,7 @@ const texts: Text[] = [
   {
     "category": "crew-safety-briefing",
     "trigger": {"event": "flightStateChange", "value": ['FLIGHT_TAXI_PRE_TAKEOFF']},
-    "timeout": [20, 30],
+    "timeout": [120, 180],
     "texts": [
       {
         "en": "Ladies and gentlemen, on behalf of the crew I ask that you please direct your attention to the crew members as we review the emergency procedures. There are {aircraftEmergencyExistsCount} emergency exits on this aircraft. Take a minute to locate the exit closest to you. Note that the nearest exit may be behind you. Should the cabin experience sudden pressure loss, stay calm and listen for instructions from the cabin crew. Oxygen masks will drop down from above your seat. Place the mask over your mouth and nose, like this. Pull the strap to tighten it. If you are traveling with children, make sure that your own mask is on first before helping your children. In the unlikely event of an emergency landing and evacuation, leave your carry-on items behind. Life rafts are located below your seats and emergency lighting will lead you to your closest exit. We ask that you make sure that all carry-on luggage is stowed away safely during the flight. While we wait for take off, please take a moment to review the safety data card in the seat pocket in front of you. Thank you for your attention.",
